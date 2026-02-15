39cc3feeec41e82f47b28576452bd66903e280ab
Nguyen Viet Huynh <dau8uuuuuu@gmail.com>
2026-02-15 22:56:44 +0700

    Khai bao va duyet mang

---
+++ Cxx.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int a[100000];
+int main(){
+    int n ;
+    scanf("%d", &n);
+    for(int i = 0 ; i < n ; i++){
+        scanf("%d", &a[i]);
+    }
+    for(int i = 0 ; i < n ; i++){
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
