c54bd18c8b36ad0ef5b997436f2943e15155a252
Nguyen Viet Huynh <dau8uuuuuu@gmail.com>
2026-02-17 23:28:39 +0700

    Them bai tap Cxx

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,10 +1,22 @@
-#include <stdio.h>
-void dequy(){
-    printf("Hello Nguyen Viet Huynh\n");
-    dequy();
-    printf("Xin chao Nguyen Viet Huynh");
-}
+#include <stdio.h>   
 int main(){
-    dequy();
-    return 0;
+    int n, a[1000];
+    scanf("%d", &n);
+    for(int i = 0 ; i < n ; i++){
+        scanf("%d", &a[i]);
+    }
+    int demchan = 0, demle = 0, tongchan = 0, tongle = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] % 2 == 0){
+            ++demchan;
+            tongchan += a[i];
+        }
+        else
+            ++demle;
+            tongle += a[i];
+    }
+    printf("%d\n", demchan);
+    printf("%d\n", demle);
+    printf("%d\n", tongchan);
+    printf("%d\n", tongle);
 }
